435f2677da4bb340d6e3947958145cd98e4cd1c1
Makoto <132969568+nx2141@users.noreply.github.com>
2024-11-10 16:17:35 +0900

    feat:shadcnのタブui実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import "./App.css";
-import ShogiBoard from "./components/three/ShogiBoard";
+import ShogiBoard from "@/components/three/ShogiBoard";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/shadcn/tabs";
 
 function App() {
   return (
     <>
-      <div className="w-full h-screen bg-black">
-        <ShogiBoard />
+      <div className="justify-center flex my-3 mx-auto w-80">
+        <Tabs defaultValue="account w-80">
+          <TabsList className="justify-center flex">
+            <TabsTrigger value="将棋盤">将棋盤</TabsTrigger>
+            <TabsTrigger value="GOWEB猫">GOWEB猫</TabsTrigger>
+          </TabsList>
+          <TabsContent value="将棋盤">
+            <ShogiBoard />
+          </TabsContent>
+          <TabsContent value="GOWEB猫">準備中...</TabsContent>
+        </Tabs>
       </div>
     </>
   );
