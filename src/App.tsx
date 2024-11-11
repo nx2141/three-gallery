ae2437263837c82f44f7dfca242c9161584a5d37
Makoto <132969568+nx2141@users.noreply.github.com>
2024-11-12 01:50:38 +0900

    style:defaultValueをgowebCatに変更

--- src/App.tsx
+++ src/App.tsx
@@ -12,19 +12,19 @@ import {
 function App() {
   return (
     <div className="mx-auto my-5 flex h-96 max-w-[850px] justify-center overflow-hidden sm:h-[600px]">
-      <Tabs defaultValue="account" className="w-full">
+      <Tabs defaultValue="GOWEB猫" className="w-full">
         <TabsList className="relative z-20 flex w-full justify-center">
-          <TabsTrigger value="将棋盤">将棋盤</TabsTrigger>
           <TabsTrigger value="GOWEB猫">GOWEB猫</TabsTrigger>
+          <TabsTrigger value="将棋盤">将棋盤</TabsTrigger>
         </TabsList>
-        <TabsContent value="将棋盤" className="h-full w-full">
-          <div className="flex h-full w-full items-center justify-center bg-slate-200">
-            <ShogiBoard />
-          </div>
-        </TabsContent>
         <TabsContent value="GOWEB猫" className="h-full w-full">
           <div className="flex h-full w-full items-center justify-center bg-slate-200">
             <GowebCat />
+          </div>
+        </TabsContent>
+        <TabsContent value="将棋盤" className="h-full w-full">
+          <div className="flex h-full w-full items-center justify-center bg-slate-200">
+            <ShogiBoard />
           </div>
         </TabsContent>
       </Tabs>
